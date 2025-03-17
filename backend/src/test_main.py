b28bec94bf7d8f118d25fe7d9b6add44a0339aed
lukas <lukas@maier-assenheim.de>
2025-03-17 19:19:00 +0100

    ASE-14 add: testcase

--- backend/src/test_main.py
+++ backend/src/test_main.py
@@ -1,12 +1,12 @@
 from fastapi.testclient import TestClient
 
-from api.main import app
+from main import app
 
 client = TestClient(app)
 
 
 def test_hello_world():
     response = client.get("/")
-    assert response.status_code == 200  # Statuscode sollte 200 OK sein
+    assert response.status_code == 200
     # Antwort sollte das erwartete JSON sein
     assert response.json() == {"message": "Hello World 🌍"}
