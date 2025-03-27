1e853b743bdd83dbf13002a573212d781bbeb7b2
Justus Siegert <jubesie@googlemail.com>
2025-03-28 00:37:26 +0100

    ASE-18 resolve conflicts

--- ai_training/src/helperscripts/confusion_analyse.py
+++ ai_training/src/helperscripts/confusion_analyse.py
@@ -15,7 +15,6 @@ import json
 import pandas as pd
 import os
 
-with open("./dataset/plantnet_300K/plantnet300K_species_names.json", "r", encoding="utf-8") as f:
 with open(os.path.abspath(
         os.path.join(
             os.path.dirname(__file__), "../../dataset/plantnet_300K/plantnet300K_species_names.json"
