ee9e1fc50c79163fad7395f19f95d04e23c6abe5
Justus <91261422+Juqsi@users.noreply.github.com>
2025-03-27 12:11:21 +0100

    ASE-18 add ai_training to Sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,6 +7,8 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../../backend/src'))
+sys.path.insert(0, os.path.abspath('../../ai_training/src'))
+
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
